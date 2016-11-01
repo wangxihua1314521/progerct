7cfc129de03e707c8363fe440797c546632bb326
wang903 <842726562@qq.com>
2016-11-01 22:10:23 +0800

    3333

--- servers/app.js
+++ servers/app.js
@@ -3,13 +3,16 @@ var express = require('express');
 //实例化
 var app = express();
 
+var path = require("path");
+
 //加载 token 模块
-require("./token")(app);
+require("./token.js")(app);
 
 
-var server = require('http').Server(app);
-server.listen(80, "0.0.0.0", function () {
-	var _host  = server.address().address;
-  	var _port  = server.address().port;
-  	console.log('Example app listening at http://%s:%s', _host, _port);
-});
+var saticpath = path.join(__diranme,"../static");
+
+app.use(express.static(saticpath));
+
+app.listen(80,,function(){
+	console.log("open http://127.0.0.1:80");
+})
